2c2789d9aaa6a36a6306fd5f849401b0f7d7979b
KaceGG <90222686+KaceGG@users.noreply.github.com>
2022-09-07 01:53:32 +0700

    Update bai5_3.c

--- bai5_3.c
+++ bai5_3.c
@@ -49,3 +49,56 @@ int main(){
         
     return 0;
 }
+_____________________________________________________________________________________________________________________________________________________________________
+#include <stdio.h>
+
+#define MAX_M 500 //So cung toi da ma do thi co the chua
+
+typedef struct{ //CTDL Edge bieu dien 1 cung (u,v)
+	int u, v; //Moi cung luu dinh dau u, dinh cuoi v
+}Edge;
+
+typedef struct{ //CTDL Graph bieu dien 1 do thi
+	int n, m; //n dinh, m cung
+	Edge edges[MAX_M]; //mang luu tru cac cung;
+}Graph;
+
+void init_graph(Graph *pG, int n, int m){
+	pG->n = n;
+	pG->m = 0;
+}
+
+void add_edge(Graph *pG, int u, int v){
+	pG->edges[pG->m].u = u;
+	pG->edges[pG->m].v = v;
+	pG->m++;		
+}
+
+int degree(Graph *pG, int u){ //bac cua dinh bang so dinh ke cua dinh do
+	int i, deg_u = 0;
+	for(i = 0; i < pG->m; i++){
+		if(pG->edges[i].u == u) deg_u++;
+		if(pG->edges[i].v == u) deg_u++;
+	}
+	return deg_u;
+}
+
+int main() {
+    Graph G;
+    int n, m, e, u, v;
+    //Mở file dothi.txt để đọc dữ liệu
+    FILE *file = fopen("dt.txt", "r");
+    //Đọc số đỉnh và số cung & khởi tạo đồ thị
+    fscanf(file, "%d%d", &n, &m);
+    init_graph(&G, n, m);
+    //Đọc m cung
+    for (e = 0; e < m; e++) {
+        fscanf(file, "%d%d", &u, &v);
+        add_edge(&G, u, v);
+    }
+    
+    for (u = 1; u <= n; u++)
+        printf("deg(%d) = %d\n", u, degree(&G, u));
+    //Đóng file
+    fclose(file);
+}
