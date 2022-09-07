32409b0bd304a76a8ebcda4112f679f240c96491
KaceGG <90222686+KaceGG@users.noreply.github.com>
2022-09-07 23:57:34 +0700

    Add files via upload

---
+++ Bai13_5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAX_N 100
+
+typedef struct{
+	int n, m;
+	int A[MAX_N][MAX_N];
+}Graph;
+
+void init_graph(Graph *pG, int n){
+	pG->n = n;
+	pG->m = 0;
+	
+	int u, v;
+	for(u = 1; u <= n; u++){
+		for(v = 1; v <= n; v++)
+			pG->A[u][v] = 0;
+	}
+}
+
+int main() {
+    Graph G;
+    int n, u, v;
+    
+    scanf("%d",&n);
+    init_graph(&G,n);
+    
+    for(u = 1; u <= n; u++){
+    	while(1){
+    		scanf("%d",&v);
+    		if(v == 0) break;
+    		G.A[u][v]++;
+		}
+	}
+	
+    for(u = 1; u <= G.n; u++){
+    	for(v = 1; v <= G.n; v++){
+    		printf("%d ",G.A[u][v]);
+		}
+		printf("\n");	
+	}
+	
+    return 0;
+}
